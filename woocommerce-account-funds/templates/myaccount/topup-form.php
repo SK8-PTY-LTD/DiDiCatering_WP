e14c1fb5d6677605388e68dda5c97dd62872cb81
Dwight <baomp2015@gmail.com>
2019-03-18 12:25:19 +1100

    display topup rules

--- woocommerce-account-funds/templates/myaccount/topup-form.php
+++ woocommerce-account-funds/templates/myaccount/topup-form.php
@@ -1,4 +1,13 @@
 <form method="post">
+	<!-- START TOPUP RULES-->
+	<label for="topup_rules"><?php _e( 'Top-up Rules', 'woocommerce-account-funds' ); ?></label>
+	<p>
+		If one-time top-up amount greater than or equal to <br/>
+		<span style="padding-left:20px;"><strong>A$500</strong>, your account will upgrade to <strong>Sliver Member</strong> and have <strong>5%</strong> discount for further purchases.</span><br/>
+		<span style="padding-left:20px;"><strong>A$2000</strong>, your account will upgrade to <strong>Golden Member</strong> and have <strong>10%</strong> discount for further purchases.</span><br/>
+		<span style="padding-left:20px;"><strong>A$5000</strong>, your account will upgrade to <strong>Platinum Member</strong> and have <strong>15%</strong> discount for further purchases.</span>
+	</p>
+	<!-- END TOPUP RULES-->
 	<label for="topup_amount"><?php _e( 'Top-up Account Funds', 'woocommerce-account-funds' ); ?></label>
 	<p class="form-row form-row-first">
 		<input type="number" class="input-text" name="topup_amount" id="topup_amount" step="0.01" value="<?php echo esc_attr( $min_topup ); ?>" min="<?php echo esc_attr( $min_topup ); ?>" max="<?php echo esc_attr( $max_topup ); ?>" />
